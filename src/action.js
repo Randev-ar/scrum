5f724bcc9302faf43fcc6491a1d94834dd5ecc83
Facundo Toffolo Pasquini <f.toff.pas@gmail.com>
2022-09-29 00:36:05 -0300

    test yml

--- src/action.js
+++ src/action.js
@@ -1,5 +1,8 @@
-async function run (){
+const core = require("@actions/core")
+const github = require("@actions/github")
 
+async function run (){
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
 }
 
 run()
