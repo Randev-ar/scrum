49f71d3f34bec75d9fbceb05094d8ea2d18d2c3b
Facundo Toffolo Pasquini <f.toff.pas@gmail.com>
2022-09-29 00:44:16 -0300

    Comentario en US

--- dist/index.js
+++ dist/index.js
@@ -9670,6 +9670,15 @@ const github = __nccwpck_require__(5438)
 
 async function run (){
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+    const octokit = github.getOctokit(GITHUB_TOKEN)
+    
+    const { context = {} } = github
+    const { issue } = context.payload
+    await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: issue.number,
+        body: 'Updating Tasks'
+    })
 }
 
 run()
--- src/action.js
+++ src/action.js
@@ -3,6 +3,15 @@ const github = require("@actions/github")
 
 async function run (){
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+    const octokit = github.getOctokit(GITHUB_TOKEN)
+    
+    const { context = {} } = github
+    const { issue } = context.payload
+    await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: issue.number,
+        body: 'Updating Tasks'
+    })
 }
 
 run()
